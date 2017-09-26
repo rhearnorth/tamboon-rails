c84e2c76772047189309af275a26cd19867e916c
North Ronnakit Vijidboonchuvong <north@nanameue.jp>
2017-09-26 22:16:06 +0900

    Refactor donation process

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,64 +1,32 @@
 class WebsiteController < ApplicationController
+
   def index
-    @token = nil
   end
 
   def donate
-    charity = Charity.find_by(id: params[:charity])
-    if params[:omise_token].present?
-      unless params[:amount].blank? || params[:amount].to_i <= 20
-        unless !charity
-          if Rails.env.test?
-            charge = OpenStruct.new({
-              amount: params[:amount].to_i * 100,
-              paid: (params[:amount].to_i != 999),
-            })
-          else
-            charge = Omise::Charge.create({
-              amount: params[:amount].to_i * 100,
-              currency: "THB",
-              card: params[:omise_token],
-              description: "Donation to #{charity.name} [#{charity.id}]",
-            })
-          end
-          if charge.paid
-            charity.credit_amount(charge.amount)
-          end
-        else
-          @token = retrieve_token(params[:omise_token])
-          flash.now.alert = t(".failure")
-          render :index
-          return
-        end
-      else
-        @token = retrieve_token(params[:omise_token])
-        flash.now.alert = t(".failure")
-        render :index
-        return
-      end
-    else
-      @token = nil
-      flash.now.alert = t(".failure")
-      render :index
-      return
-    end
-    if !charity
-      @token = nil
-      flash.now.alert = t(".failure")
-      render :index
-      return
-    end
-    if charge.paid
+    donation = Donation.new(donation_params)
+
+    if donation.valid? && donation.paid
+      charity.credit_amount(donation.satang_amount)
       flash.notice = t(".success")
-      redirect_to root_path
     else
-      @token = nil
+      @token = retrieve_token(params[:omise_token])
       flash.now.alert = t(".failure")
-      render :index
     end
+    render :index
   end
 
   private
+
+  def charity
+    @charity ||= begin
+      if params[:charity] == 'random'
+        Charity.order("RANDOM()").first
+      else
+        Charity.find_by(id: params[:charity])
+      end
+    end
+  end
 
   def retrieve_token(token)
     if Rails.env.test?
@@ -75,5 +43,13 @@ class WebsiteController < ApplicationController
     else
       Omise::Token.retrieve(token)
     end
+  end
+
+  def donation_params
+    {
+      omise_token: params[:omise_token],
+      amount: params[:amount].to_i,
+      charity: charity
+    }
   end
 end
---
+++ app/models/donation.rb
@@ -0,0 +1,32 @@
+class Donation
+  include ActiveModel::Model
+  attr_accessor :omise_token, :amount, :charity
+
+  validates :omise_token, presence: true
+  validates :charity, presence: true
+  validates :amount, numericality: { greater_than_or_equal_to: 20 }
+
+  def paid
+    charge.paid
+  end
+
+  def satang_amount
+    amount.to_i * 100
+  end
+
+  def charge
+    @charge ||= if Rails.env.test?
+      OpenStruct.new({
+        amount: satang_amount,
+        paid: (amount.to_i != 999),
+      })
+    else
+      Omise::Charge.create({
+        amount: satang_amount,
+        currency: "THB",
+        card: omise_token,
+        description: "Donation to #{charity.name} [#{charity.id}]",
+      })
+    end
+  end
+end
