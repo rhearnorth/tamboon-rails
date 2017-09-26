585437c5d13d81ecfcb982d05950308dfcd4f470
North Ronnakit Vijidboonchuvong <north@nanameue.jp>
2017-09-26 22:16:32 +0900

    Prevent racing condition

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -2,7 +2,9 @@ class Charity < ActiveRecord::Base
   validates :name, presence: true
 
   def credit_amount(amount)
-    new_total = total + amount
-    update_attribute :total, new_total
+    self.with_lock do
+      new_total = total + amount
+      update_attribute :total, new_total
+    end
   end
 end
